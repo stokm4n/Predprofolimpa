874db3426b731bfbe859b301f885ba42d4afcbf2
11T30 <s_fyodorov2006@mail.ru>
2024-02-26 17:17:07 +0300

    3 задание

---
+++ 3 задание.py
@@ -0,0 +1,19 @@
+import csv
+with open('game.csv', encoding='utf8') as file:
+    reader = list(csv.reader(file, delimiter='$'))[1:]
+    ans = []
+    name = input()
+    while name != 'game':
+        for gameName, characters, nameError, date in reader:
+            if name == characters:
+                ans.append(gameName)
+        print(*ans[:5])
+        break
+    if len(ans) == 0:
+        print('Этого персонажа не существует')
+
+
+        name = input()
+
+
+
