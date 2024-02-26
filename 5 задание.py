482b7706fe7a16cb2c9805bf0ccc650625f31611
11T30 <s_fyodorov2006@mail.ru>
2024-02-26 17:52:47 +0300

    5 задание

--- 5 задание.py
+++ 5 задание.py
@@ -1,15 +1,18 @@
 import csv
+
+
 def generate_cashe(s: str):
     alphabet = 'йцукенгшщзхъфывапролджэячсмтьбю ЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ'
     d = {n: i for i, n in enumerate(alphabet, 1)}
     p = 65
-    m = 10**9 + 9
+    m = 10 ** 9 + 9
     hash_value = 0
     p_pow = 1
     for c in s:
         hash_value = (hash_value + d[c] * p_pow) % m
         p_pow = (p_pow * p) % m
     return int(hash_value)
+
 
 with_hash = []
 with open('game.csv', encoding='utf8') as file:
