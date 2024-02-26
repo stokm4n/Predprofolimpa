0f19206279cdaab11f2e03d0cc0fe1a75a37e632
11T30 <s_fyodorov2006@mail.ru>
2024-02-26 17:51:14 +0300

    3 задание

---
+++ 5 задание.py
@@ -0,0 +1,24 @@
+import csv
+def generate_cashe(s: str):
+    alphabet = 'йцукенгшщзхъфывапролджэячсмтьбю ЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ'
+    d = {n: i for i, n in enumerate(alphabet, 1)}
+    p = 65
+    m = 10**9 + 9
+    hash_value = 0
+    p_pow = 1
+    for c in s:
+        hash_value = (hash_value + d[c] * p_pow) % m
+        p_pow = (p_pow * p) % m
+    return int(hash_value)
+
+with_hash = []
+with open('game.csv', encoding='utf8') as file:
+    reader = list(csv.reader(file, delimiter='$'))[1:]
+    for row in reader:
+        row[0] = generate_cashe(row[1])
+        with_hash.append(row)
+
+with open('game.csv', 'w', encoding='utf8', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(['GameName,characters,gameError,date'])
+    writer.writerows(with_hash)
