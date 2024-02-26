3583eaf3d84d00dbf5fdeb5b9319c4e01f4288af
11T30 <s_fyodorov2006@mail.ru>
2024-02-26 17:16:12 +0300

    1 задание

---
+++ 1 задание.py
@@ -0,0 +1,17 @@
+import csv
+
+with open('game.csv', encoding='utf8') as file:
+    reader = list(csv.reader(file, delimiter='$'))[1:]
+
+    for gameName, characters, nameError, date in reader:
+        if '55' in nameError:
+            print(f'У персонажа {characters} в игре {gameName} нашлась ошибка с кодом {nameError}. Дата фиксации: {date}')
+    for row in reader:
+        if '55' in row[2]:
+            row[2] = 'Done'
+            row[3] = '0000-00-00'
+
+with open('game_new.csv', 'w', encoding='utf8', newline='') as file:
+    writer = csv.writer(file, delimiter='$')
+    writer.writerow(['GameName' , 'characters' , 'nameError', 'date'])
+    writer.writerows(reader)
